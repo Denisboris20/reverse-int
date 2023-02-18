fce22bb0e818e44f51fa4b81e2563bd9de917d3b
Denisboris20 <denisboris20@gmail.com>
2023-02-18 19:19:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let reversed = n
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+        .replace(/^0*||0*$/, "");
+
+    return n < 0 ? reversed.slice(0, -1) : reversed;
+};
